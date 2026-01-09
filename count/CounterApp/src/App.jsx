7e6fde0561a62afed0f5b7b6055e5d4c8780792f
omuphade77 <omuphade77@gmail.com>
2026-01-09 15:18:45 +0530

    Counter App Done

---
+++ count/CounterApp/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
